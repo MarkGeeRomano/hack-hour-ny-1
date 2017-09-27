939eb24da2dd8f8a99fb8c970535e9c9ca635754
George 2.0 Hope <PracticalCode@users.noreply.github.com>
2017-09-27 10:07:59 -0400

    Completed & Tested 20170927
    Description
    Completed & Tested 20170927
    by George 2.0 ☺

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,5 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble 
+//      by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -16,8 +17,27 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {
 
+
+function fizzbuzz(num) {
+    const newArray = [];
+    for(let i = 1; i <= num; i++){
+        if((i % 3 == 0) && (i % 5 == 0))
+            newArray.push('fizzbuzz');
+        else {
+            if((i % 3 == 0))
+                newArray.push('fizz');
+            else {
+                if ((i % 5 == 0))
+                    newArray.push('buzz');
+                else
+                    newArray.push(i);
+            }
+        }
+    }
+  return newArray;
 }
+
+//console.log("fizzbuzz(16) === " + fizzbuzz(16));
 
 module.exports = fizzbuzz;
