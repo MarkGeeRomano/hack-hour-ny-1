8c2bd9dccd6c964c33625feeb7544ae61c95e1ca
javier olayis <javier olayis>
2017-10-03 10:04:12 -0400

    firstPull

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -1,5 +1,6 @@
 /*
- * Given an array of numbers, determine if the mode and mean of the array are equivalent
+ * Given an array of numbers, 
+ * determine if the mode and mean of the array are equivalent
  *
  * Caveats:
  * 	- Math.floor the mean
@@ -12,6 +13,22 @@
 
 function modemean(array) {
 
+	let mean;
+	let temp;
+	for (let i  = 0; i < array.length;  i++) {
+		temp=temp+array[i];
+	}
+	c(temp)
+	mean=temp/array.length;
+	c(mean)
+	let mode =2;
+	return mean ==mode;
+
 }
 
-module.exports = modemean;
+c( modemean([2,2,2]) )
+
+function c(n){
+  console.log(n);
+}
+//module.exports = modemean;
