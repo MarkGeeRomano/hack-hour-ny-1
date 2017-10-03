313e7387cb87f72276d1cb859b745cb459977dcc
Aum Dutta <saurabhaumdutta@gmail.com>
2017-10-03 10:04:31 -0400

    kth to the node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -15,14 +15,43 @@
  *
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
 
 function Node(val) {
   this.value = val;
   this.next = null;
 }
-
+// let node = new Node(val);
 function kthToLastNode(k, head) {
+  let node = new Node(val);
+  // get a length of the linked list
+  let length = this.length;
+  // declar a curr variable dn intialize it to the head
+  let current = head;
+  //check if number k is les than 1 or not a number, if so return undefined;
+  if (typeof k !== 'number' || k < 1) return undefined;
+  //iterate through the linked list, loop through until current is undefined
+  while (current) {
+    length += 1;
+    current = current.next;
+  }
+  // define index of the value kth to last 
+  let i = length - k;
+  current = head;
 
+  while (i) {
+    current = current.next;
+    i -= 1;
+  }
+  return current.value;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
+  // we will create two variable count, mean and max we will intialize value 0 to them.
+  // we will iterate through the array and sum all number
+  // also we we will find min and max numbers in the array
+  // for mean we will find sum of all of the numbers in array and divide by length of the array
+  // as we iterate throught the array we can create object property(num) and value(count)
+  // we will find our mode, which is property with high count, we return our !(mean - mode === 0)
+  let count = 0;
+  let mean = 0;
+  let max = 0;
+  let obj = {};
+  let length = array.length;
+  for (let i = 0; i < length; i++) {
+    let sum += array[i];
+    obj[array[i]] === undefined ? count++ obj[array[i]] = count : obj[array[i]] += count;
+    if (count > max) {
+      max = count;
+      for (let key in obj) {
+          mode = obj[key];
+      }
+    }
 
+  }
+  }
+  mean = Math.floor(sum / length);
+  return !(mode - mean === 0);
 }
 
 module.exports = modemean;
