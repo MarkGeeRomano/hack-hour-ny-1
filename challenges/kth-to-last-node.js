e83f0cbee51e44ce9e62f33028a4360e64f5ab88
Julie Moon <8juliemoon@gmail.com>
2017-10-04 09:49:40 -0400

    complete challenges

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -8,12 +8,16 @@
  * const d = new Node('D');
  * const e = new Node('E');
  *
- * a.next = b;
+ * a.next = b;// we are creating the link
  * b.next = c;
  * c.next = d;
  * d.next = e;
  *
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
+ */
+ /*
+ find the length of the node and then length-k
+
  */
 
 function Node(val) {
@@ -21,12 +25,49 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
-//find the length of the number of nodes you have (looping)
-//have a counter keeping track of k; k = 0;
- //if this.next === null go back once; ++k;
- //check counter; if === k then return node[value];
+function kthToLastNode1(k, head) {
+ let len = 0//length can be 1
 
+ // let currNode = ; LET is block scope so we can't use currNode inside for loop. 
+ //Block scope means you can't use it outside of the loop.
+ let currNode = head;
+ //as long as currNode exists because it is an object and that's truthy
+ for(; currNode; currNode = currNode.next){//gets to end of the list to get the length
+ 	len++;
+ }
+ console.log('The list has',len,'nodes');
+ //2nd loop is to loop through the nodesreusing currNode and reset it to the head
+ currNode = head;
+ for(let i = 0; i<len-k; i++){
+ 	currNode = currNode.next;
+ }
+ return currNode.value;
 }
+//console.log(kthToLastNode1(1,a))//E
+//console.log(kthToLastNode1(2,a))//D
+
+	//need to get to the end of the list
+	//strategy is to use two pointers: fastNode( finds length) slowNode(identifies value of node you want to return)
+	//offset = k-1 . offset fastNode by K *offset is how far they are from each other
+	//When k = 1 they aren't offset but when k=2 fastnode is(B)
+//THIS ONE WILL HAVE CONSTANT RUN TIME but the FIRST ONE will be slower:
+function kthToLastNode2(k, head){
+ let slowNode = head;
+ let fastNode = head;
+ //offset
+ for(let i = 0; i<k; i++){
+ 	fastNode = fastNode.next;
+ 	//this breaks the offset
+ }
+ //while fastNode exists
+ while(fastNode){
+ 	fastNode = fastNode.next;
+ 	slowNode = slowNode.next;
+ }
+ return slowNode.value;
+}
+//if k is 5
+//console.log(kthToLastNode2(1,a))//E
+
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
