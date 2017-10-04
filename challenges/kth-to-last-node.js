cd3ab6bd40f67a589d7cb90f674c92da04d32416
Chris <cli53@binghamton.edu>
2017-10-04 10:01:30 -0400

    pow completed

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,42 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+// let len = 0;
+// let currNode = head;
+//if currNode is truthy, under the condition of the code block, assign currNode to currNode.next
+//Objects evaluate to truthy
+//First loop is for checking the length of the of the Linked List
+// for(; currNode;currNode = currNode.next) {
+//   len += 1;
+// }
+// console.log('The list has', len, 'nodes');
 
+
+// currNode = head;
+// for(let i = 0; i < len -k; i++) {
+//   currNode = currNode.next;
+// }
+
+// return currNode.value;
+
+let slowNode = head;
+let fastNode = head;
+// if k is more than len, it becomes null, but if it's e.g 2, it's just 2 ahead of the slowNode that starts at 0;
+for(let i = 0; i < k; i += 1) {
+  fastNode = fastNode.next;
 }
+
+//If fastNode is not null (exceeding the length), it both nodes will start at their respective positons,
+//fastNode being k ahead of slowNode;
+while(fastNode) {
+  fastNode = fastNode.next;
+  slowNode = slowNode.next;
+}
+return slowNode.value;
+};
+
+
+
+
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,8 @@
  */
 
 function pow(base, power) {
-
+    if(power === 0) return 1
+    return base * pow3(base, --power)
 }
 
 module.exports = pow;
