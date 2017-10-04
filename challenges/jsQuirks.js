e6b647dbb51ff05e8db7040ba7bf7ba637bc775d
George 2.0 Hope <PracticalCode@users.noreply.github.com>
2017-10-04 09:55:07 -0400

    Completed pow(base, power) Updated Files

---
+++ challenges/jsQuirks.js
@@ -0,0 +1,79 @@
+
+/*
+Javascript Quirks Quiz #1
+Please do not run the code in your terminal. Think critically about the answer.
+
+Name ~ George Hope
+
+1) Is JavaScript case sensitive language? 
+Yes	.	.	.	.	.	.	.	.	.	.	.	.	.	.	.	.	.	<<< [correct]
+No
+
+*/
+/*
+*/
+let testOutput = (function(){
+    return typeof arguments;
+})();
+
+console.log("testOutput[typeof:" + typeof testOutput + "] === " + testOutput);
+/*
+
+2) What will be the output of the above code snippet? 
+"object"
+"array"
+"arguments"
+"undefined"
+
+3) What will be the output of the above code snippet? 
+1
+null
+undefined
+Error
+
+4) What will be the output of the above code snippet? 
+1
+"number"
+undefined
+"undefined"
+
+5) What will be the output of the above code snippet? 
+"undefined"
+"object"
+"number"
+"function"
+
+6) What will be the output of the above code snippet? 
+1
+"1function"
+"1undefined"
+NaN
+
+7) What will be the output of the above code snippet? 
+"number"
+"string"
+"undefined"
+"object"
+
+8) What will be the output of the above code snippet? 
+1
+2
+Error
+undefined
+
+9) What will be the output of the above code snippet? 
+"undefined"
+"object"
+undefined
+null
+
+10) What will be the output of the above code snippet? 
+"a defined? true" / "b defined? true"
+"a defined? false" / "b defined? true"
+"a defined? true" / "b defined? false"
+"a defined? false" "b defined? false"
+SUBMIT
+Never submit passwords through Google Forms.
+This content is neither created nor endorsed by Google. Report Abuse - Terms of Service - Additional Terms
+Google Forms
+*/
--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,24 @@
  */
 
 function pow(base, power) {
-
+    if (power === 0)
+      return 1;
+    else if (power === 1)
+      return base;
+    else if (power > 1)
+      return base * pow(base, power - 1);
 }
+
+// let currBase;
+// let currPower;
+// currBase = 2;
+// currPower = 0;
+// console.log("pow(" + currBase + ", " + currPower + ") === " + pow(currBase, currPower++));
+// console.log("pow(" + currBase + ", " + currPower + ") === " + pow(currBase, currPower++));
+// console.log("pow(" + currBase + ", " + currPower + ") === " + pow(currBase, currPower++));
+// console.log("pow(" + currBase + ", " + currPower + ") === " + pow(currBase, currPower++));
+// console.log("pow(" + currBase + ", " + currPower + ") === " + pow(currBase, currPower++));
+
+
 
 module.exports = pow;
