627664382b5759f7bdf9ecb434f77a5ca048806d
javier olayis <javier olayis>
2017-10-04 09:56:11 -0400

    firstCommit

--- challenges/pow.js
+++ challenges/pow.js
@@ -1,9 +1,21 @@
-/* Write a function that calculates x^y, where x is given as the base and y is given as the power.
+/* Write a function that calculates x^y, 
+where x is given as the base and y is given as the power.
  * Use recursion!
  */
 
 function pow(base, power) {
+	let result=0;
+	for (var i =1 ; i < power-1 ; i++) {
+			base=base*base
+			c(base);
+	}
 
+	//c(base);
+	return result;
 }
+pow(2,5)
 
+function c(argument) {
+	console.log(argument)
+}
 module.exports = pow;
