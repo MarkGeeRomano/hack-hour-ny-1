0e3a547f9f89ca3b37241a6c66a33ebcd2b9a268
George 2.0 Hope <PracticalCode@users.noreply.github.com>
2017-10-06 15:34:30 -0400

    Forgot to upload... Reverse in place...

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -13,8 +13,56 @@
  * DO NOT USE THE BUILT IN REVERSE METHOD
  */
 
+// Easy & safe...  for loop...
 function reverseInPlace(array) {
-
+  if (array.length < 2) {
+    return array;
+  } else {
+    for (let currIdx = 0; currIdx < array.length / 2; currIdx++) {
+      [array[currIdx], array[array.length - 1 - currIdx]] = [array[array.length - 1 - currIdx], array[currIdx]];
+    }
+    return array;
+  }
 }
+
+// Trickier...  recursion...
+function reverseInPlace(array, currIdx = 0) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    // console.log("____________________________________");
+    console.log("____________________________________");
+    console.log("reverse idx's  " + currIdx + " & " + (array.length - 1 - currIdx));
+    [array[currIdx], array[array.length - 1 - currIdx]] = [array[array.length - 1 - currIdx], array[currIdx]];
+    console.log("array === " + array);
+    currIdx += 1;
+    let blnNeedRecursion = Math.floor(array.length / 2);
+    if (currIdx < blnNeedRecursion) {
+      return reverseInPlace(array, currIdx);
+    }
+    return array;
+  }
+}
+
+let testArray;
+console.log("////////////////===>>>>>>>>>>>>>>>");
+testArray = [];
+console.log("reverseInPlace(" + testArray + ") === " + reverseInPlace(testArray));
+console.log("////////////////===>>>>>>>>>>>>>>>");
+testArray = [0];
+console.log("reverseInPlace(" + testArray + ") === " + reverseInPlace(testArray));
+console.log("////////////////===>>>>>>>>>>>>>>>");
+testArray = [0, 1];
+console.log("reverseInPlace(" + testArray + ") === " + reverseInPlace(testArray));
+console.log("////////////////===>>>>>>>>>>>>>>>");
+testArray = [0, 1, 2];
+console.log("reverseInPlace(" + testArray + ") === " + reverseInPlace(testArray));
+console.log("////////////////===>>>>>>>>>>>>>>>");
+testArray = [0, 1, 2, 3];
+console.log("reverseInPlace(" + testArray + ") === " + reverseInPlace(testArray));
+console.log("////////////////===>>>>>>>>>>>>>>>");
+testArray = [0, 1, 2, 3, 4];
+console.log("reverseInPlace(" + testArray + ") === " + reverseInPlace(testArray));
+console.log("////////////////===>>>>>>>>>>>>>>>");
 
 module.exports = reverseInPlace;
