b7bef7e73259c2505300cb61357f0449ce942576
Ben Israel S. Suarez <benisraels.suarez@Estrellas-MacBook-Pro.local>
2017-10-07 22:59:31 -0400

    submit bin to dec no ext

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,29 @@
  */
 
 function binToDec(binary) {
+  var digits = binary.split('');
+  var powers = digits.length;
+  var base = 2;
+  var sum = 0;
 
+  if (digits[0] === 1) {
+    digits[0] = 1;
+  }
+
+  for (var i = 0; i < powers; i++) {
+    var currBase = base *= 2
+    if (Number(digits[powers - (i + 1)]) === 1) {
+      Number(digits[powers - (i + 1)]) = currBase;
+    }
+  }
+
+  for (var k = 0; k < digits.length; k++) {
+    if (digits[k]) {
+      sum += digits[k];
+    }
+  }
+
+  return sum;
 }
 
 module.exports = binToDec;
