ce5e42226ad61a8d0d7df9e1cc6373c33ce0bc9c
javier olayis <javier olayis>
2017-10-13 10:03:04 -0400

    first commit

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,4 +1,8 @@
-// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or punctuation.
+// Some languages have "if" statements that are closed by "fi" instead of curly brackets. 
+//Or they close a "case" with "esac", i.e. the same keyword backwards. 
+//for this problem we'll check that all words in a string are "closed".
+// Write a function that takes a string and returns true if every word is 
+//closed by its backwards counterpart. Words must be separated by space or punctuation.
 
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
@@ -9,6 +13,16 @@
 
 function matchWord(str) {
 
+	let c={end : "nde" , if: "fi" , for:"rof"  , while : "elihw" , try: "try"}
+	console.log(str)
+	for (var i = 0; i < str.length; i++) {
+		let s=str[i].toLowerCase()
+		console.log(s);
+		
+	}
+return true;
 }
 
-module.exports = matchWord;
+matchWord('__END_DNE-----'); 
+
+//module.exports = matchWord;
