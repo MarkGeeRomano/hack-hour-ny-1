631dd8656c13112bdcc578ef5e9a2344bf05062c
jq-chang <jqw.chang@gmail.com>
2017-10-18 10:00:25 -0400

    highest-product complete

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
+    // if (array[0] < 0 && array[1] < 0) {
+    //     if (Math.abs(array[0] + array [1]) > (array[array.length - 1] + array[array.length - 2])) {
+    //     }
+    // }
 
+    if (array.length === 3) return array.reduce((product, num) => product * num);
+
+    array.sort((a, b) => a - b);
+    return highestProduct(array.slice(-3));
 }
-
 
 module.exports = highestProduct;
