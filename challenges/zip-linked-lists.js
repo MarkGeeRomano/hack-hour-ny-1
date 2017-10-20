56775ccdd5f3b376d9260a7554fee9dc5527fd13
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-10-20 11:08:11 -0400

    Finished---- i think

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,21 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  currNodel1 = l1;
+  currNodel2 = l2;
+  tempNode;
+  while (currNodel2.next !== null) {
+    // store the rest of list
+    tempNode = currNodel1.next;
+    // set the next val to list 2's node
+    currNodel1.next = currNodel2;
+    // add rest of list to newly appended node
+    currNodel2.next = tempNode;
+
+    currNodel1 = currNodel1.next.next;
+    currNodel2 = currNodel2.next;
+  }
+  return l1;
 };
 
 module.exports = {Node: Node, zip: zip};
