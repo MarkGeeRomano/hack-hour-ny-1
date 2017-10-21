129d0b68d3d2ac4a3aa430d902c8e83d6cd5494e
George 2.0 Hope <PracticalCode@users.noreply.github.com>
2017-10-21 09:58:56 -0400

    completed Stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -1,3 +1,5 @@
+//  CS-HackHours-Stack
+
 /*
  * Create a stack with the push, pop, and getMax methods.
  * push should return the new length of the stack.
@@ -6,8 +8,70 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
-  // body...
+class Stack {
+  constructor () {
+    this.maxVal;
+    this.stack = [];
+  }
+  
+  length () {
+    return this.stack.length;
+  }
+  
+  push (newVal) {
+    this.stack.push(newVal);
+    if (this.maxVal) { 
+      (newVal > this.maxVal) ? this.maxVal = newVal : null;
+    } 
+    else this.maxVal = newVal;
+    
+    return this.stack.length;
+  }
+  
+  newMax() {
+    if (this.stack.length >= 1) {
+      this.maxVal = this.stack[0];
+      for (let i = 1; i <= this.stack.length - 1; i += 1)
+        if (this.stack[i] > this.maxVal) this.maxVal = this.stack[i];
+        
+    }
+    else this.maxVal = undefined;
+  }
+  
+  pop () {
+    let returnVal;
+    if (this.stack.length > 0) {
+      returnVal = this.stack.pop();
+      if (returnVal === this.maxVal) this.newMax();
+    }
+    return returnVal;
+  }
+  
+  getMax () {
+    return this.maxVal;
+  }
 }
+
+
+const myStack = new Stack();
+
+console.log(myStack.push(13));
+console.log("myStack.getMax () === ", myStack.getMax ());
+console.log(myStack.push(69));
+console.log("myStack.getMax () === ", myStack.getMax ());
+console.log(myStack.push(42));
+console.log("myStack.getMax () === ", myStack.getMax ());
+
+console.log(myStack.pop());
+console.log("myStack.getMax () === ", myStack.getMax ());
+console.log(myStack.pop());
+console.log("myStack.getMax () === ", myStack.getMax ());
+console.log(myStack.pop());
+console.log("myStack.getMax () === ", myStack.getMax ());
+console.log(myStack.pop());
+
+
+
+
 
 module.exports = Stack;
