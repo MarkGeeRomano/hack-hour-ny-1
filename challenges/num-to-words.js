c8c8c95e8347b7773cc503c24d98934b831cd96d
javier olayis <javier olayis>
2017-10-27 09:59:54 -0400

    done

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -1,5 +1,7 @@
-// Write a function that takes a number as an argument and returns its english word representation as a string. Answers should be in upper camel case (a.k.a. Pascal case). Don't worry about spaces.
-// Include support for 11-19 ('Eleven', 'Twelve', 'Thirteen', ... 'Nineteen').
+// Write a function that takes a number as an argument and returns its 
+/*english word representation as a string. Answers should be in 
+upper camel case (a.k.a. Pascal case). Don't worry about spaces.
+*/// Include support for 11-19 ('Eleven', 'Twelve', 'Thirteen', ... 'Nineteen').
 
 
 /**
@@ -13,7 +15,40 @@
  */
 
 function numToWords(num) {
+	//console.log(num);
+	
+    let ones= ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+    let tens= ['', '', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+    let sep= ['', ' thousand ', ' million ', ' billion ', ' trillion ', ' quadrillion ', ' quintillion ', ' sextillion ']
 
+    let str='';
+    let i=0;
+    let newArry=[];
+
+
+    while(num){
+      newArry.push(num % 1000);
+      num=parseInt(num/1000, 10);
+
+    }
+
+    while(arr.length){
+	  str+=( ()=>{
+
+	  	let a=Math
+
+	  }
+
+
+
+
+	  	)( arr.shift() )+ sep[i++]+str;   
+    }
+
+	//return num.toString();	
+	return str; 
 }
 
+console.log(numToWords(2) ); //-> 'Zero'
+console.log(numToWords(92120000000000000) );// -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
 module.exports = numToWords;
