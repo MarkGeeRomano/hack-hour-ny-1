f550b4e12efc4e384e9c57b96fb43b9d4a007e4a
jq-chang <jqw.chang@gmail.com>
2017-10-28 09:19:31 -0400

    permPalin done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,11 @@
  */
 
 function permPalin(str) {
-	
+  if (str.length === 1) return true; // a
+  if (str.length === 3 && str[0] === str[2] && str[0] !== str[1]) return true; // aba  
+  if (str.length === 3 && str[0] !== str[2]) return false;
+  
+  return permPalin(str.slice(1));
 }
 
 module.exports = permPalin;
