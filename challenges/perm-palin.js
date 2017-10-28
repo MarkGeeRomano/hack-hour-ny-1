a5205961f0ec64bfc3dd27db69f2296b68ad7fa6
javier olayis <javier olayis>
2017-10-28 09:48:43 -0400

    done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -1,5 +1,6 @@
 /**
- * Given a string, determine if any of the permutations of that string is a palindrome
+ * Given a string, determine if any of 
+ the permutations of that string is a palindrome
  *
  * Example:
  * 	- permPalin('abab') => true
@@ -10,7 +11,22 @@
  */
 
 function permPalin(str) {
-	
-}
+	let normalizedStr=str.toLowerCase().split(' ').join('').split('');
+	let  cache={};
+	let isEven=normalizedStr.length % 2 ===0;
+	let oddCount=0;
 
+	for (var i = 0; i < normalizedStr.length; i++) {
+		cache[ normalizedStr[i] ] ? cache[  normalizedStr[i]  ]++ : cache[  normalizedStr[i] ]=1;
+	}
+
+	for(let key in cache){
+		if( cache[key] % 2!==0 ){oddCount++;}
+		if( cache[key] % 2!==0 && isEven ){return false;}
+		if( cache[key] % 2!==0 && oddCount >1  & !isEven){return false;}
+	}
+
+	return true;
+}
+console.log( permPalin( "abab" )  )
 module.exports = permPalin;
