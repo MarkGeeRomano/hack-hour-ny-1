100bfb4fa607f5cd5736719cceaf400165397b11
javier olayis <javier olayis>
2017-10-30 10:00:57 -0400

    done

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -1,4 +1,5 @@
-/* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents 
+/* You are given a NxN grid of elements 
+	represented by a 2D array. The ith nested array represents 
  * the ith row in the grid.
  * 
  * Write a function to rotate the grid 90 degrees clockwise. 
@@ -18,6 +19,42 @@
 
 function rotateGrid(grid, n) {
 
+	/*
+
+	for(i)
+	for(j)
+
+	*/
+	
+	for (let i = 0; i < grid.length; i++) {
+		let temp=grid[i][0];
+		for (let j = grid[i].length-1; j < ; j++) {
+			temp=grid[i][j];
+			
+		}
+	}
+
 }
 
+/*
+
+  x	 y 		 x  y
+{ 0, 0 }=> { 0, 2 }
+{ 0, 1 }=> { 1, 2 }
+{ 0, 3 }=> { 2, 2 }
+
+{ 1, 0 }=> { 0, 1 }
+{ 1, 1 }=> { 1, 1 }
+{ 1, 2 }=> { 2, 1 }
+
+{ 2, 0 }=> { 0, 0 }
+{ 2, 1 }=> { 1, 0 }
+{ 2, 2 }=> { 2, 0 }
+
+
+*/
+
+
+
+//console.log(  rotateGrid([   [1, 2, 3],[4, 5, 6],[7, 8, 9]  ]),3 ) 
 module.exports = rotateGrid;
