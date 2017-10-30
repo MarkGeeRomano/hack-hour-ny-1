26500373be2cbf19f73b9dccc551432357a000a4
George 2.0 Hope <PracticalCode@users.noreply.github.com>
2017-10-30 10:01:11 -0400

    completed

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -1,3 +1,5 @@
+//  CS-HackHours-numToWords
+
 // Write a function that takes a number as an argument and returns its english word representation as a string. Answers should be in upper camel case (a.k.a. Pascal case). Don't worry about spaces.
 // Include support for 11-19 ('Eleven', 'Twelve', 'Thirteen', ... 'Nineteen').
 
@@ -12,8 +14,147 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
 
+const EnglishNumeralMap = {
+	"1000000000000000": "Quadrillion",
+	"1000000000000": "Trillion",
+	"1000000000": "Billion",
+	"1000000": "Million",
+	"1000": "Thousand",
+	"100": "Hundred",
+	"90": "Ninety",
+	"80": "Eighty",
+	"70": "Seventy",
+	"60": "Sixty",
+	"50": "Fifty",
+	"40": "Forty",
+	"30": "Thirty",
+  "20":"Twenty",
+  "19":"Nineteen",
+  "18":"Eighteen",
+  "17":"Seventeen",
+  "16":"Sixteen",
+  "15":"Fifteen",
+  "14":"Fourteen",
+  "13":"Thirteen",
+  "12":"Twelve",
+  "11":"Eleven",
+  "10":"Ten",
+  "9":"Nine",
+  "8":"Eight",
+  "7":"Seven",
+  "6":"Six",
+  "5":"Five",
+  "4":"Four",
+  "3":"Three",
+  "2":"Two",
+  "1":"One",
 }
+
+const EnglishNumeralZero = {
+  "0":"Zero",
+}
+
+const EnglishNumeralDescendingKeyVal = [1000000000000000, 1000000000000, 1000000000, 1000000, 1000, 100, 90, 80, 70, 60, 50, 40, 30, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+
+// console.log ("Object.keys(EnglishNumeralMap).length === " + Object.keys(EnglishNumeralMap).length);
+// console.log ("EnglishNumeralDescendingKeyVal.length === " + EnglishNumeralDescendingKeyVal.length);
+
+
+function englishNumeralSub100(num, iENKV = 0) {
+  // console.log ("______________________________");
+  // console.log ("IN EnglishNumeral iENKV === " + iENKV);
+  numChars = Math.floor(num / EnglishNumeralDescendingKeyVal[iENKV]);
+  // console.log ("IN EnglishNumeral numChars === " + numChars);
+  num -= EnglishNumeralDescendingKeyVal[iENKV] * numChars;
+  // console.log ("IN EnglishNumeral new num === " + num);
+  if (num === 0) {
+    return Array(numChars+1).join(EnglishNumeralMap[EnglishNumeralDescendingKeyVal[iENKV]]);
+  } else {
+    return Array(numChars+1).join(EnglishNumeralMap[EnglishNumeralDescendingKeyVal[iENKV++]]) + englishNumeralSub100(num, iENKV);
+  }
+}
+
+function englishNumeral(num) {
+  // return englishNumeralSub100(num);
+  let strNumerals = '';
+  if (Number.isSafeInteger(num)) {
+    if (num === 0) {
+      strNumerals = EnglishNumeralZero[0];
+    }
+    let subNumber;
+    let iENKV = 0;
+    while (num) {
+      switch (true) {
+        case (num >= 1000) :
+          subNumber = Math.floor(num / EnglishNumeralDescendingKeyVal[iENKV]);
+          if (subNumber) {
+            strNumerals += englishNumeral(subNumber) + EnglishNumeralMap[EnglishNumeralDescendingKeyVal[iENKV]];
+            num = num % EnglishNumeralDescendingKeyVal[iENKV];
+          }
+          iENKV++;
+          break;
+        case (num >= 100) :
+          subNumber = Math.floor(num / EnglishNumeralDescendingKeyVal[iENKV]);
+          if (subNumber) {
+            strNumerals += englishNumeralSub100(subNumber) + EnglishNumeralMap[EnglishNumeralDescendingKeyVal[iENKV]];
+            num = num % EnglishNumeralDescendingKeyVal[iENKV];
+          }
+          iENKV++;
+          break;
+        default :
+          strNumerals += englishNumeralSub100(num);
+          num = 0;
+      }
+    }
+  }
+  else {
+    strNumerals = num + ' is NOT a safe integer.';
+  }
+  return strNumerals
+}
+
+let numToWords = englishNumeral;
+
+let testNum;
+
+// testNum = 0;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 3;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 4;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 5;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 6;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 8;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 9;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 10;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 11;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 800;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 900;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 1000;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 1100;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
+testNum = Number.MAX_SAFE_INTEGER; // 9007199254740991
+console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+testNum = Number.MAX_SAFE_INTEGER + 1; // 9007199254740991
+console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 999888777666555392;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+// testNum = 999888777666222080;
+// console.log ("englishNumeral(testNum=" + testNum + ") === " + englishNumeral(testNum));
+
 
 module.exports = numToWords;
--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -1,3 +1,5 @@
+//  CS-HackHours-rotateGrid
+
 /* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents 
  * the ith row in the grid.
  * 
@@ -16,8 +18,54 @@
  * BONUS: Do this in place
  */
 
-function rotateGrid(grid, n) {
+console.log("________________________________")
 
+//init the grid matrix
+function Array2D(numCols, numRows) {
+  let new2dArray = [];
+  for (let iRows = 0; iRows < numRows; iRows++) {
+    let newMatrix = [];
+    for ( let iCols = 0; iCols < numCols; iCols++ ) {
+      newMatrix[iCols] = []; 
+    }
+    
+    new2dArray.push(newMatrix);
+  }
+  return new2dArray;
 }
+
+function fillArray2D(array, start=0) {
+  let value = start;
+  for (let iRows = 0; iRows < array[0].length; iRows++) {
+    for ( let iCols = 0; iCols < array.length; iCols++ ) {
+      array[iCols][iRows] = value++; 
+    }
+  }
+}
+
+let n = 3;
+let my2dArray = Array2D(n, n);
+console.log("my2dArray === ", my2dArray)
+fillArray2D(my2dArray);
+console.log("my2dArray === ", my2dArray)
+
+console.log("________________________________")
+
+
+function rotateGrid(grid, n) {
+  for (let iOuter = 0; iOuter <= grid.length - 1; iOuter += 1) {
+    for (let iInner = iOuter + 1; iInner <= grid.length - 1; iInner += 1) {
+      console.log("iOuter, iInner === ", iOuter, iInner);
+      [grid[iOuter][iInner] , grid[iInner][iOuter]] = [grid[iInner][iOuter] , grid[iOuter][iInner]];
+    }
+  }
+}
+
+console.log("________________________________")
+rotateGrid(my2dArray, n);
+console.log("my2dArray === ", my2dArray)
+
+
+
 
 module.exports = rotateGrid;
