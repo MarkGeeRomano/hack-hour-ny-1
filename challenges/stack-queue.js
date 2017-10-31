2ce9462918d6d571fa833b7b09e9925d26d69bb5
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-10-31 10:05:36 -0400

    Done

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,6 +4,18 @@
 
 
 function Stack() {
+  this.storage = {};
+  this.length = 0;
+}
+Stack.prototype.push = function(val) {
+  this.storage[this.length++] = val;
+  return this.length;
+}
+Stack.prototype.pop = function() {
+  const val = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length -= 1;
+  return val;
 
 }
 
@@ -14,7 +26,22 @@ function Stack() {
 
 
 function Queue() {
+  this.in = new Stack();
+  this.out = new Stack();
 
+}
+
+Queue.prototype.enqueue = function(val) {
+  this.in.push(val);
+}
+
+Queue.prototype.dequeue = function() {
+  let popped = this.in.pop();
+  while (popped) {
+    this.out.push(popped);
+    popped = this.in.pop();
+  } 
+  return this.out.pop();
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
