3075840dd1be4ac88d0c57fd42fe1656d245ada0
jq-chang <jqw.chang@gmail.com>
2017-10-31 09:41:20 -0400

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,14 +4,22 @@
 
 
 function Stack() {
+  this.storage = {};
+  this.length = 0;
 
+  this.prototype.push = (val) => {
+    this.storage[this.length++] = val;
+  };
+
+  this.prototype.pop = () => {
+    let temp = this.storage[--this.length];
+    delete this.storage[this.length];
+    return temp;
+  };
 }
-
-
 /**
 * Queue Class
 */
-
 
 function Queue() {
 
