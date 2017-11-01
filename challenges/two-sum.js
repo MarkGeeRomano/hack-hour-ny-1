1c0b88af05237962342dc1f689d8bd8910b0c79a
jq-chang <jqw.chang@gmail.com>
2017-11-01 10:06:11 -0400

    twooooooooo-sum in the hizzzzzzzzzouse

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,13 @@
  */
 
 function twoSum(arr, n) {
+  const cache = {};
+  for (var i = 0; i < arr.length; i++) {
+    cache[arr[i]] = n - arr[i];
+    if (cache[arr[i]] === arr[i]) return true;
+  }
 
+  return false;
 }
 
 module.exports = twoSum;
