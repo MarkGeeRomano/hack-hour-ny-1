3f01f6ef50549f5f7c3232ac7ed269e0c24accda
javier olayis <javier olayis>
2017-11-02 10:00:21 -0400

    done

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,16 +4,36 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
+ //https://articles.leetcode.com/determine-if-binary-tree-is-binary/
+ //has a good solution
+
+function leaf(val){
+	this.value=null;
+	this.left=null;
+	this.right=null;
+} 
 
 function BinaryTree(val) {
     this.value = val;
     this.left = null;
     this.right = null;
 }
+BinaryTree.prototype.addLeaf=function( leaf ){
+
+	if(leaf.value <this.value ){
+		this.left= new leaf(leaf.value);
+	}else{
+		this.right=new leaf(leaf.value);
+	}
+
+};
 
 function validBST(tree) {
 
 }
+
+
+let t= BinaryTree(42);
+
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
