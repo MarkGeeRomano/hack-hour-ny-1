01b151a68495a17770e46550d53eec867876c88c
javier olayis <javier olayis>
2017-11-04 09:58:26 -0400

    done

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -6,9 +6,40 @@
  * gcd(10, 9) -> 1
  *
  */
+/*
+function gcd(a, b) {
+
+	//divide each num until you get an irrational number
+	//save each divisor into an cache array
+	//once both numbers become irrational after dividing 
+	//return the highest number from the highest array which is length-1
+
+	let divisor=1;
+	let ary=[];
+	while ( a<1 && b< 1 ){
+		ary.push(ary[divisor])
+		a=a/divisor;
+		b=b/divisor;
+		if( a <    )
+
+	}
+
+}*/-
+
+
 
 function gcd(a, b) {
 
+	//divide each num until you get an irrational number
+	//save each divisor into an cache array
+	//once both numbers become irrational after dividing 
+	//return the highest number from the highest array which is length-1
+	if(!b){
+		return a;
+	}
+
+	return gcd( b, a%b );
 }
 
+//console.log( gcd( 10, 8 ) );
 module.exports = gcd;
