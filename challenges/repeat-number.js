0377a0697e66aa82eca005d7577bd6f011eb90dc
javier olayis <jolaya182@gmail.com>
2017-11-17 09:45:20 -0500

    done

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -1,5 +1,6 @@
 /**
- * I have an array where every number in the range 1...n appears once except for 
+ * I have an array where every number 
+ in the range 1...n appears once except for 
  * one number which appears twice.
  *
  * Write a function for finding the number that appears twice.
@@ -9,9 +10,27 @@
  * Do this in O(1) space
  *
  */
-
+//1,2,3,4,5,6,7,8,9,10,11,2,12,
 function repeatNumbers(array) {
+	let num="";
+	let len=array.length;
+	for (var i = 0; i < len; i++) {
+		let a = Math.abs( array[i] );
+		console.log(a)
+		if( a   >= 0 ){
+			array[Math.abs( array[i] )] = -array[Math.abs( array[i] )];
+		} else{
+			num = " "+ array[i]+", "
+		}
+
+	}
+
+	console.log(num);
+	return num
+
 
 }
 
+
+repeatNumbers( [1,2,3,4,5,6,7,8,9,10,11,2,12,13])
 module.exports = repeatNumbers;
