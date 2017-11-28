03ff82e5ceadc98e9a4f97df538509e26e7c5e71
javier olayis <jolaya182@gmail.com>
2017-11-28 09:46:49 -0500

    done

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,48 @@
 
 
 function deleteDups(head) {
+	let temp = head
+	let seen = {};
+	let out = [];
+	let j = -1;
+	let ll = "";
+	console.log(temp)
+	while(temp.next != null){
 
+		if( seen[temp.value] !== 1 ){
+			seen[temp.value] = 1;
+			out[j++] = temp.value;
+			ll.next =  new node(temp.value)
+
+		} 
+		temp = temp.next;
+
+
+	}
+	//console.log(out)
+return ll;
 }
+
+function node( value ){
+	this.value = value;
+	this.next = null;
+}
+
+let head  = new node(4);
+let a = new node(3);    
+let b = new node(5);  
+let c = new node(17); 
+let d =  new node(0);
+let e = new node(17);
+let f = new node(5); 
+head.next = a;
+a.next = b
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+
+//console.log(head);
+console.log(deleteDups(head));
 
 module.exports = deleteDups;
