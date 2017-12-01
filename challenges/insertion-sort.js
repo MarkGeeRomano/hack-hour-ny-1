133ebf6eefcf1bb2c22cbdd5f2622b33d1694212
Ben Israel S. Suarez <benisraels.suarez@Estrellas-MacBook-Pro.local>
2017-12-01 10:07:27 -0500

    potential solution use two while loops

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,21 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  let i = 1;
+  
+  while (i < array.length) {
+    let x = array[i];
+    let j = i -1;
+    while (j >= 0 && array[j] > x) {
+      array[j+1] = array[j]
+      j = j-1;
+    }
+    array[j+1] = x;
+    i = i+1;
+  }
+  
+  return array;
 }
 
 module.exports = insertionSort;
