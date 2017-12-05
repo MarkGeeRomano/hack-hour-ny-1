ee8c887c2ef3533ea94e27b180a95a77299e2f90
Chris <cli53@binghamton.edu>
2017-12-05 12:16:16 -0500

    doubly-linked-list completed

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,47 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  const node = new Node(val);
+
+  if(!this.head) {
+    this.head = node;
+    this.tail = node;
+  }
+  else {
+    node.prev = this.tail;
+    this.tail.next = node;
+    this.tail = node;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if(this.head.value === value) {
+    this.head.next.prev = null;
+    this.head = this.head.next;
+    return this.head;
+  }
+  else {
+    let currNode = this.head;
+    let nodeExist = true;
+    while(nodeExist) {
+      if(currNode.value === val) {
+          if(currNode.next.value === null) {
+            curr.next.next = null;
+          }
+          else {
+          currNode.prev.next = currNode.next;
+          currNode.next.prev = currNode.prev;
+          nodeExist = false;
+          }
+      }
+      else {
+      currNode = currNode.next;
+      }
+    }
+  }
 };
 
 module.exports = LinkedList;
