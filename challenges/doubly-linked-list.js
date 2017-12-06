565cc3d1a728eb1819950927a9d4b3333e0329fd
jq-chang <jqw.chang@gmail.com>
2017-12-06 10:02:56 -0500

    doubly

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -8,7 +8,7 @@ function LinkedList() {
 }
 
 function Node(val) {
-  this.val = val;
+  this.value = val;
   this.next = null;
   this.prev = null;
 }
@@ -17,14 +17,36 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if (!this.head) {
+    this.head = new Node(val);
+    this.tail = this.head;
+  } else {
+    let curr = new Node(val);
+    this.tail.next = curr;
+    curr.prev = this.tail;
+    this.tail = curr;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (this.head.value === val) {
+    this.head.next.prev = null;
+    this.head.next = this.head;
+  } 
+
+  for (let curr = this.head.next; curr; curr = curr.next) {
+    if (curr.value === val) {
+      if (curr !== this.tail) {
+        curr.prev.next = curr.next;
+        curr.next.prev = curr.prev;
+      } else { // it is a tail
+        curr.prev.next = null;
+      }
+    }
+  }
 };
 
 module.exports = LinkedList;
