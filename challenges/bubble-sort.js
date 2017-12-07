5d7bbca2df33c77a695292a02cc35f3aa1831d27
shachyjrclear <shachyj.rivas@gmail.com>
2017-12-07 09:43:20 -0500

    Finished bble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+  let lastInd = array.length;
+  while (lastInd > 0) {
+    for (let i = 0; i < lastInd; i += 1) {
+      if (array[i] > array[i + 1]) {
+        [ array[i], array[i + 1]] = [ array[i + 1], array[i]];
+      }
+    }
+    lastInd--;
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
