2d1be93508a3bd434144cd11f8f6d34d1289a07b
shachyjrclear <shachyj.rivas@gmail.com>
2017-12-07 14:06:48 -0500

    edits to bbl sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -4,17 +4,51 @@
 
 
 
+// function bubbleSort(array) {
+//   let lastInd = array.length;
+//   let swapped = true;
+//   let count = 0;
+//   while (swapped && lastInd > 0) {
+//     swapped = false;
+//     for (let i = 0; i < lastInd; i += 1) {
+//       count++;
+//       if (array[i] > array[i + 1]) {
+//         console.log(`Swapped: ${array[i]} => ${array[i+1]}`);
+//         [array[i], array[i + 1]] = [array[i + 1], array[i]];
+//         swapped = true;
+//       }
+//       // console.log(array[i]);
+//     }
+//     lastInd--;
+//   }
+//   console.log(count);
+//   return array;
+// }
 function bubbleSort(array) {
   let lastInd = array.length;
-  while (lastInd > 0) {
+  let count = 0;
+  let swapped = false;
+  do {
+    swapped = false;
     for (let i = 0; i < lastInd; i += 1) {
+      count++;
       if (array[i] > array[i + 1]) {
-        [ array[i], array[i + 1]] = [ array[i + 1], array[i]];
+        // console.log(`Swapped: ${array[i]} => ${array[i+1]}`);
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        swapped = true;
       }
+      // console.log(array[i]);
     }
     lastInd--;
-  }
+    console.log(array);
+  } while (swapped && lastInd > 0);
+  console.log(count);
   return array;
 }
+
+// [38,39,40,41]
+// bubbleSort([41,36,37,38, 39,40]);
+// bubbleSort([1,2,3]);
+// bubbleSort([]);
 
 module.exports = bubbleSort;
