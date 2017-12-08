6fb5460824a20d9d423602f34443e00879c57ad9
javier olayis <jolaya182@gmail.com>
2017-12-08 09:40:59 -0500

    done

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -27,16 +27,50 @@
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
 
+	console.log(jazbook[0][0])
+	// loop through the array
+	for (let i = 0; i < jazbook.length; i++) {
+	// for each element check for the name to match at element i[j][0]
+		//for (let j = 0; j < jazbook.length; j++) {
+			console.log(jazbook[i])
+	// if so return the number(value) of that matched name 
+			//if( name === "") return true;		
+		//}
+	}
+	// 	if not then simple return false after looping through
+	return false;
 }
 
 // return an object literal representing the jazbook
 function makePhoneBookObject(jazbook){
+	let hashtable = {};
+	// loop through the array of arrays
+	for (let i = 0; i < jazbook.length; i++) {
+	// get the first name and the phone number into a hashtable
+		hashtable[jazbook[i]] = jazbook[i];
+	}
+	// return the hashtable
+	return hashtable;
+	
 
 }
+
+   jazbook = [
+     ['alex','301-844-3421'],
+     ['jae','301-844-1211'],
+     ['david','301-844-0978'],
+     ['travis','301-844-8505']
+     ['jasmine','1800-974-4539'],
+   ];
+console.log(jazbook[0][1]); // 1
+console.log(findName(jazbook, "jasmine") );
+console.log(makePhoneBookObject(jazbook) );
 
 const objectToExport = {
   findName,
   makePhoneBookObject,
 };
+
+
 
 module.exports = objectToExport;
