d90a99d61e9af84790c0c43f247c21569a932249
javier olayis <jolaya182@gmail.com>
2017-12-20 09:35:42 -0500

    done

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -10,7 +10,36 @@
 */
 
 function solveKnapsack(items, weightAvailable) {
+	
+	// intialize variable that tract the current value count and weight count
+	let currentValue = 0;
+	let currentWeightCount = 0;
+	let betterValue = 0;
+	let betterWeightCOunt = 0;
+	// go through array of objects
+	for(var i = 0; i < items.length; i++) {
+	  items[i]
+	// for every object in the array verify if the wieght as exceeded 
+	  if( items[i].weight + currentWeightCount <= weightAvailable  ){ 
+	    currentWeightCount = items[i].weight + currentWeightCount;
+	    currentValue = currentValue + items[i].value;	
+	   }else{
+		// 	if not then return the value count
+	   	return currentValue;
+	   }
+
+	// 	if the weight has exceeded then return the current value count
+	}
+	//if there is a better wheight count then update the betterWeightCOunt anb bette value
+
+	// after the loop is done return the value count		
+	return currentValue;
+
 
 };
+
+let items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+  console.log(solveKnapsack(items, 3)); // returns 7 (from items[0] and items[1])
+  console.log(solveKnapsack(items, 5)); // returns 9 (from items[1] and items[2])
 
 module.exports = solveKnapsack;
