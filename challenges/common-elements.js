55e5b9831050ae1963e7b12705c17ed76ae2d6fc
javier olayis <jolaya182@gmail.com>
2017-12-22 09:42:28 -0500

    done

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -38,7 +38,7 @@ function commonElements(array1, array2, array3, array4){
   		for (let j = 0; j < allArrays[i].length; j++) {
 	    //if they have a match then push it into a new array
   			if( eachArry[i]  ===  allArrays[i][j] )
-	  		newAccArry.push(eachArry[i]);
+	  		 newArray[i]=[eachArry[i]];
 	    //if they don't match continue to the next element
   		}
  
@@ -53,5 +53,11 @@ function commonElements(array1, array2, array3, array4){
     // otherwise return nothing in commin as a defautlt result
 	return "Nothing in Common!";
 }
+
+  var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+console.log(commonElements(array1, array2, array3, array4  ) )
 
 module.exports = commonElements;
--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -1,8 +1,10 @@
 /*
-Write a function that takes two arguments, an array and a callback. The callback will be called once for each permutation
+Write a function that takes two arguments, an array and a callback. The 
+callback will be called once for each permutation
 of the elements of the array, where the permutation is passed as an argument.
 
-Permutations are all the possible rearrangements of the elements. For this problem, assume each permutation must include
+Permutations are all the possible rearrangements of the elements. For this 
+problem, assume each permutation must include
 every element from the array.
 
 Permutations must NOT repeat any of the array elements.
@@ -22,8 +24,29 @@ eachPermutation([1, 2, 3], function(perm) {
 
 function eachPermutation(arr, callback) {
 
+	//use nested for loop
+	// outter loop controls each number
+	for (let i = 0; i < arr.length; i++) {
+	// inner loop controls every permutation
+		for (let j = 0; j < arr.length; j++) {
+	// have a condition taht checks when the current indedx is checking against itself from outter to inner
+			if( i === j ){ continue}
+    // if so skip if not continue searching
+			console.log(  arr[i ], " : ", arr[j] )	
+
+			
+		}
+	}
+    // for each permuation get the that number and send it to the callback function
+	
+
+
 }
 
 
 
+
+eachPermutation([1, 2, 3], function(perm) {
+  console.log(perm)
+});
 module.exports = eachPermutation;
